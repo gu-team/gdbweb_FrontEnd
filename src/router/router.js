f7f2da7d4f02a1665a9c37ea548b5800f1c93d84
99MyCql <bug@nuaa.edu.cn>
2019-05-21 15:13:07 +0800

    modify router

--- src/router/router.js
+++ src/router/router.js
@@ -4,12 +4,12 @@ import Vue from 'vue'
 // 在Vue中加载 VueRouter
 Vue.use(VueRouter)
 
-import Home from '@/view/home.vue'
+import Home from '@/view/home'
 
 // 将该VueRouter实例导出
 export default new VueRouter({
     // 该属性名为 routes 而非 routers，害苦我也
     routes: [
-        { path: '/home', name: 'Home', meta: {title: 'Home - 主页'}, component: Home }
+        { path: '/', name: 'Home', meta: {title: 'Home - 主页'}, component: Home }
     ]
 })
